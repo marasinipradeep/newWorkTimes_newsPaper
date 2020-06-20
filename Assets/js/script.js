3935099b9417a5e3d424c113078757e5a73270f0
Chris Roschi <chrisroschi53@gmail.com>
2020-06-20 13:48:01 +0930

    working code with clear function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,48 +8,58 @@ var searchTerm = "";
 
 
 $("#searchButton").on("click", function () {
-  
+
   event.preventDefault();
   searchTerm = $("#searchTerm").val();
   console.log(searchTerm);
-  
+
   beginDate = $("#startYear").val();
   console.log(beginDate);
-  
+
   endDate = $("#endYear").val();
   console.log(endDate);
 
   var selectedNumber = $("#numberOfRecordsToRetrieve").val();
-  console.log(selectedNumber); 
-  
+  console.log(selectedNumber);
+
   var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?begin_date=" + beginDate + "&end_date=" + endDate + "&q=" + searchTerm + "&api-key=" + APIkey;
   console.log(queryURL);
-  
-  
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (result) {
-      console.log(result);
-      
-      for (var i = 0; i < selectedNumber; i ++) {
 
-          var article = $("<div>");
-          
-          var title = $("<h3>");
-          title.text(i + ". " + result.response.docs[i].headline.main);
+  clearResults();
 
-          var author = $("<p>");
-          author.text(result.response.docs[i].byline.original);
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (result) {
+    console.log(result);
 
-          var lineBreak = $("<hr>");
-          
-          article.append(title, author, lineBreak);
-          article.addClass("articleDiv");
-          $("#results").append(article);
-      }
-    });
-  
+    for (var i = 0; i < selectedNumber; i++) {
+
+      var article = $("<div>");
+
+      var title = $("<h3>");
+      title.text(i + ". " + result.response.docs[i].headline.main);
+
+      var author = $("<p>");
+      author.text(result.response.docs[i].byline.original);
+
+      var link = $("<a>");
+      link.append(title);
+      link.attr("href", result.response.docs[i].web_url);
+
+      var lineBreak = $("<hr>");
+
+      article.append(link, author, lineBreak);
+      article.addClass("articleDiv");
+      $("#results").append(article);
+    }
+  });
+
 });
 
+$("#clearButton").on("click", clearResults);
+
+function clearResults() {
+  $("#results").empty();
+}
 
